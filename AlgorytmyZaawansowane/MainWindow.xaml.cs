ee51c14c8f82ff66974373b96c7931a4fb9085a1
Łukasz <draganl@student.mi.pw.edu.pl>
2016-03-05 17:10:41 +0100

    trochę gui

---
+++ AlgorytmyZaawansowane/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Shapes;
+
+namespace AlgorytmyZaawansowane
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool isStarted = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        Polygon polygon;
+        Point startPoint;
+        Point firstPoint;
+        Point endPoint;
+        Line currentLine;
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton != MouseButtonState.Pressed)
+                return;
+            if (!isStarted)
+            {
+                if (e.ClickCount == 1)
+                {
+                    isStarted = true;
+                    firstPoint = e.GetPosition(this);
+                    startPoint = firstPoint;
+                    endPoint = firstPoint;
+                    polygon = new Polygon { firstPoint };
+                    CreateNewLine();
+                }
+            }
+            else
+            {
+                if (e.ClickCount == 1)
+                    endPoint = e.GetPosition(this);
+                else
+                {
+                    isStarted = false;
+                    startPoint = endPoint;
+                    endPoint = firstPoint;
+                    Task.Factory.StartNew(()=> { MessageBox.Show(polygon.ToString()); });
+                }
+                UpdateCurrentLine();
+            }
+        }
+
+        private void CreateNewLine()
+        {
+            currentLine = new Line
+            {
+                X1 = startPoint.X,
+                Y1 = startPoint.Y,
+                X2 = endPoint.X,
+                Y2 = endPoint.Y,
+                Stroke = SystemColors.WindowFrameBrush,
+                //StrokeThickness = 3
+            };
+            paintSurface.Children.Add(currentLine);
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                if (isStarted)
+                {
+                    endPoint = e.GetPosition(this);
+                    UpdateCurrentLine();
+                }
+            }
+        }
+
+        private void UpdateCurrentLine()
+        {
+            currentLine.X2 = endPoint.X;
+            currentLine.Y2 = endPoint.Y;
+        }
+
+        private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (isStarted)
+            {
+                startPoint = endPoint;
+                CreateNewLine();
+                if(endPoint!=polygon[polygon.Count-1])
+                    polygon.Add(endPoint);
+            }
+        }
+    }
+}
---
+++ AlgorytmyZaawansowane/Polygon.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows;
+
+namespace AlgorytmyZaawansowane
+{
+    class Polygon : List<Point>
+    {
+        public Polygon()
+        {
+
+        }
+        //public bool IsPointInside(Point point)
+        //{
+        //    bool isInside = false;
+        //    for (int i = 0, j = Count - 1; i < Count; j = i++)
+        //    {
+        //        if (((this[i].Y > point.Y) != (this[j].Y > point.Y)) &&
+        //        (point.X < (Points[j].X - Points[i].X) * (point.Y - Points[i].Y) / (Points[j].Y - Points[i].Y) + Points[i].X))
+        //        {
+        //            isInside = !isInside;
+        //        }
+        //    }
+        //    return isInside;
+        //}
+        public bool IsSimple()
+        {
+            throw new NotImplementedException();
+        }
+        public double GetArea()
+        {
+            var area = Math.Abs(this.Take(Count - 1)
+               .Select((p, i) => (this[i + 1].X - p.X) * (this[i + 1].Y + p.Y))
+               .Sum() + (this[0].X - this[Count-1].X) * (this[0].Y + this[Count - 1].Y)) / 2.0;
+            return area;
+        }
+        public override string ToString()
+        {
+            return string.Join(", ", this) + " Area: " + GetArea();
+        }
+    }
+
+}
