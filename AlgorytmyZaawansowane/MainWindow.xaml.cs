010b16607dbfac13ea08546b25b6adb6154f12af
Łukasz <draganl@student.mi.pw.edu.pl>
2016-03-09 22:19:21 +0100

    wczytywanie z pliku i trochę issimple

--- AlgorytmyZaawansowane/MainWindow.xaml.cs
+++ AlgorytmyZaawansowane/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Shapes;
@@ -10,25 +14,31 @@ namespace AlgorytmyZaawansowane
     /// </summary>
     public partial class MainWindow : Window
     {
-        bool isStarted = false;
+        Polygon polygon;
+        Point point;
+
         public MainWindow()
         {
             InitializeComponent();
         }
-        Polygon polygon;
+        bool isDrawingStarted = false;
         Point startPoint;
         Point firstPoint;
         Point endPoint;
         Line currentLine;
+
+        public const string InputFileName = "in.txt";
+        public const string OutputFileName = "out.txt";
+
         private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.LeftButton != MouseButtonState.Pressed)
                 return;
-            if (!isStarted)
+            if (!isDrawingStarted)
             {
                 if (e.ClickCount == 1)
                 {
-                    isStarted = true;
+                    isDrawingStarted = true;
                     firstPoint = e.GetPosition(this);
                     startPoint = firstPoint;
                     endPoint = firstPoint;
@@ -42,10 +52,10 @@ namespace AlgorytmyZaawansowane
                     endPoint = e.GetPosition(this);
                 else
                 {
-                    isStarted = false;
+                    isDrawingStarted = false;
                     startPoint = endPoint;
                     endPoint = firstPoint;
-                    Task.Factory.StartNew(()=> { MessageBox.Show(polygon.ToString()); });
+                    Task.Factory.StartNew(() => { MessageBox.Show(polygon.ToString()); });
                 }
                 UpdateCurrentLine();
             }
@@ -69,7 +79,7 @@ namespace AlgorytmyZaawansowane
         {
             if (e.LeftButton == MouseButtonState.Pressed)
             {
-                if (isStarted)
+                if (isDrawingStarted)
                 {
                     endPoint = e.GetPosition(this);
                     UpdateCurrentLine();
@@ -85,12 +95,55 @@ namespace AlgorytmyZaawansowane
 
         private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            if (isStarted)
+            if (isDrawingStarted)
             {
                 startPoint = endPoint;
                 CreateNewLine();
-                if(endPoint!=polygon[polygon.Count-1])
+                if (endPoint != polygon[polygon.Count - 1])
                     polygon.Add(endPoint);
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            paintSurface.Children.Clear();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ReadInput();
+            //DrawData();
+        }
+
+        private void DrawData()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ReadInput()
+        {
+            string[] line1, line2;
+            using (var file = new System.IO.StreamReader(InputFileName))
+            {
+                line1 = file.ReadLine().Split(' ');
+                line2 = file.ReadLine().Split(' ');
+            }
+            var vertices = new List<Point>();
+
+            for (int i = 0; i < line1.Length; i += 2)
+                vertices.Add(new Point(double.Parse(line1[i]), double.Parse(line1[i + 1])));
+            polygon = new Polygon();
+            polygon.AddRange(vertices);
+
+            double x = double.Parse(line2[0]);
+            double y = double.Parse(line2[1]);
+            point = new Point(x, y);
+        }
+        private void WriteOutput()
+        {
+            using (var file = new StreamWriter(OutputFileName))
+            {
+                file.WriteLine("");
             }
         }
     }
--- AlgorytmyZaawansowane/Polygon.cs
+++ AlgorytmyZaawansowane/Polygon.cs
@@ -13,6 +13,17 @@ namespace AlgorytmyZaawansowane
         {
 
         }
+
+        private IEnumerable<Point> _vertices
+        {
+            get
+            {
+                foreach (var v in this)
+                    yield return v;
+                yield return this[0];
+            }
+        }
+
         //public bool IsPointInside(Point point)
         //{
         //    bool isInside = false;
@@ -28,18 +39,67 @@ namespace AlgorytmyZaawansowane
         //}
         public bool IsSimple()
         {
-            throw new NotImplementedException();
+            if (Count < 3) throw new InvalidOperationException("Too few vertices.");
+            var vertices = _vertices.ToList();
+            for (int i = 1; i < vertices.Count; i++)
+            {
+                for (int j = i + 1; j < vertices.Count; j++)
+                {
+                    if (DoLinesIntersect(vertices[i - 1], vertices[i], vertices[j - 1], vertices[j])) {
+                        if (j == i + 1)
+                        {
+                            if (IsBetween(vertices[i - 1], vertices[i], vertices[j]) || IsBetween(vertices[j - 1], vertices[j], vertices[i - 1]))
+                                //dwa kolejne pokrywają się
+                                return false;
+                        }else if(i == 1 && j == vertices.Count - 1)
+                        {
+                            if (IsBetween(vertices[i - 1], vertices[i], vertices[j-1]) || IsBetween(vertices[j - 1], vertices[j], vertices[i]))
+                                //pierwszy i ostatni pokrywają się
+                                return false;
+                        }
+
+                        else
+                            return false;
+                    }
+                }
+            }
+            return true;
         }
         public double GetArea()
         {
-            var area = Math.Abs(this.Take(Count - 1)
-               .Select((p, i) => (this[i + 1].X - p.X) * (this[i + 1].Y + p.Y))
-               .Sum() + (this[0].X - this[Count-1].X) * (this[0].Y + this[Count - 1].Y)) / 2.0;
+            var vertices = _vertices.ToList();
+            var area = Math.Abs(vertices.Take(vertices.Count - 1)
+               .Select((p, i) => (vertices[i + 1].X - p.X) * (vertices[i + 1].Y + p.Y))
+               .Sum() /*+ (this[0].X - this[Count-1].X) * (this[0].Y + this[Count - 1].Y)*/) / 2.0;
             return area;
         }
         public override string ToString()
         {
-            return string.Join(", ", this) + " Area: " + GetArea();
+            return string.Join(", ", this) + " Area: " + GetArea() + (IsSimple()? " SIMPLE":" NOT SIMPLE");
+        }
+        public static bool DoLinesIntersect(Point p1, Point p2, Point p3, Point p4)
+        {
+            return CrossProduct(p1, p2, p3) !=
+                   CrossProduct(p1, p2, p4) ||
+                   CrossProduct(p3, p4, p1) !=
+                   CrossProduct(p3, p4, p2);
+        }
+        public static double CrossProduct(Point p1, Point p2, Point p3)
+        {
+            return (p2.X - p1.X) * (p3.Y - p1.Y) - (p3.X - p1.X) * (p2.Y - p1.Y);
+        }
+        public static bool IsBetween(Point a, Point b, Point c)
+        {
+            var crossproduct = (c.Y - a.Y) * (b.X - a.X) - (c.X - a.X) * (b.Y - a.Y);
+            if (Math.Abs(crossproduct) > double.Epsilon) return false;
+
+            var dotproduct = (c.X - a.X) * (b.X - a.X) + (c.Y - a.Y) * (b.Y - a.Y);
+            if (dotproduct < 0) return false;
+
+            var squaredlengthba = (b.X - a.X) * (b.X - a.X) + (b.Y - a.Y) * (b.Y - a.Y);
+            if (dotproduct > squaredlengthba) return false;
+
+            return true;
         }
     }
 
