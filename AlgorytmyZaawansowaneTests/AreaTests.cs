ddf1d89a107e0ad74de39663fcd18443da8c74e6
Łukasz <draganl@student.mi.pw.edu.pl>
2016-05-07 13:57:19 +0200

    testy

---
+++ AlgorytmyZaawansowaneTests/AreaTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorytmyZaawansowane;
+using System.Windows;
+using System.Collections.Generic;
+
+namespace AlgorytmyZaawansowaneTests
+{
+    [TestClass]
+    public class AreaTests
+    {
+        [TestMethod]
+        public void Area1()
+        {
+            Polygon poli = new Polygon(new List<Point> { new Point(0, 0), new Point(0, 1), new Point(1, 1) });
+            double area = poli.GetArea();
+
+            Assert.AreEqual(0.5, area);
+        }
+        [TestMethod]
+        public void Area2()
+        {
+            Polygon poli = new Polygon(new List<Point> { new Point(0, 0), new Point(0, -1), new Point(-1, -1) });
+            double area = poli.GetArea();
+
+            Assert.AreEqual(0.5, area);
+        }
+        [TestMethod]
+        public void Area3()
+        {
+            Polygon poli = new Polygon(new List<Point> { new Point(-1, -1), new Point(0, -1), new Point(0, 0) });
+            double area = poli.GetArea();
+
+            Assert.AreEqual(0.5, area);
+        }
+        [TestMethod]
+        public void Area4()
+        {
+            Polygon poli = new Polygon(new List<Point> { new Point(-0.5, -0.5), new Point(0.5, -0.5), new Point(0.5, 0.5) });
+            double area = poli.GetArea();
+
+            Assert.AreEqual(0.5, area);
+        }
+        [TestMethod]
+        public void Area5()
+        {
+            Polygon poli = new Polygon(new List<Point>
+            {
+                new Point(0, 0),
+                new Point(5, -1),
+                new Point(5, 0),
+                new Point(5, 1),
+                new Point(4, 0),
+                new Point(4, 1),
+                new Point(3, 0),
+                new Point(3, 1),
+                new Point(2, 0),
+                new Point(2, 1),
+            });
+            double area = poli.GetArea();
+
+            Assert.AreEqual(5, area);
+        }
+    }
+}
---
+++ AlgorytmyZaawansowaneTests/IsSimpleTests.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorytmyZaawansowane;
+using System.Windows;
+using System.Collections.Generic;
+
+namespace AlgorytmyZaawansowaneTests
+{
+    [TestClass]
+    public class IsSimpleTests
+    {
+        [TestMethod]
+        public void IsSimple_Krawedz_Przecina_Krawedz()
+        {
+            Polygon poli = new Polygon(new List<Point>
+            {
+                new Point(0, 0),
+                new Point(0, 1),
+                new Point(1, 0),
+                new Point(1, 1),
+            });
+
+            bool isSimple = poli.IsSimple();
+
+            Assert.IsFalse(isSimple);
+        }
+        [TestMethod]
+        public void IsSimple_Krawedz_Pokrywa_Niesasiednia_Krawedz()
+        {
+            Polygon poli = new Polygon(new List<Point>
+            {
+                new Point(0, 0),
+                new Point(1, 0),
+                new Point(1, 1),
+                new Point(2, 0),
+            });
+
+            bool isSimple = poli.IsSimple();
+
+            Assert.IsFalse(isSimple);
+
+        }
+        [TestMethod]
+        public void IsSimple_Krawedz_Pokrywa_Sasiednia_Krawedz()
+        {
+            Polygon poli = new Polygon(new List<Point>
+            {
+                new Point(0, 0),
+                new Point(1, 0),
+                new Point(1, 1),
+                new Point(0, 1),
+                new Point(1, 1),
+            });
+
+            bool isSimple = poli.IsSimple();
+
+            Assert.IsFalse(isSimple);
+
+        }
+        [TestMethod]
+        public void IsSimple_Krawedz_Przecina_Wierzcholek()
+        {
+            Polygon poli = new Polygon(new List<Point>
+            {
+                new Point(0, 0),
+                new Point(2, 0),
+                new Point(2, 1),
+                new Point(1, 0),
+                new Point(1, 1),
+            });
+
+            bool isSimple = poli.IsSimple();
+
+            Assert.IsFalse(isSimple);
+
+        }
+        [TestMethod]
+        public void IsSimple_Wierzcholek_Pokrywa_Wierzcholek()
+        {
+            Polygon poli = new Polygon(new List<Point>
+            {
+                new Point(0, 0),
+                new Point(1, 0),
+                new Point(0.5, 0.5),
+                new Point(1, 1),
+                new Point(0.5, 0.5),
+            });
+
+            bool isSimple = poli.IsSimple();
+
+            Assert.IsFalse(isSimple);
+        }
+
+        [TestMethod]
+        public void IsSimple_JestProsty()
+        {
+            Polygon poli = new Polygon(new List<Point>
+            {
+                new Point(0, 0),
+                new Point(1, 0),
+                new Point(0.15, 0.57),
+                new Point(1, 1),
+                new Point(1, 5),
+            });
+
+            bool isSimple = poli.IsSimple();
+
+            Assert.IsTrue(isSimple);
+        }
+    }
+}
